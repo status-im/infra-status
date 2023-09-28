b88958e80435dd7784023847e262c08c8f759b2f
Anton Iakimov <yakimant@gmail.com>
2023-09-28 16:47:57 +0200

    terraform: open p2p ports in cloud firewall settings
    https://github.com/status-im/infra-shards/issues/2

--- hosts_boot.tf
+++ hosts_boot.tf
@@ -17,6 +17,9 @@ module "boot" {
   gc_type = local.ws["gc_type"] /* Google Cloud */
 
   /* firewall */
+  open_tcp_ports = [
+    "30303", /* p2p main */
+  ]
   open_udp_ports = [
     "9000", /* discovery v5 */
   ]
