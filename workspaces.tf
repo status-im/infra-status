b64ec654a53f93d8f7ff43c961561c1a06a112df
apentori <pentori.alexis@proton.me>
2025-07-24 09:11:38 +0200

    bump google cloud store and boot node
      increase RAM for each node type
    
    Signed-off-by: apentori <pentori.alexis@proton.me>

--- workspaces.tf
+++ workspaces.tf
@@ -33,11 +33,11 @@ locals {
     prod = {
       store_do_type = "s-4vcpu-8gb"
       store_ac_type = "ecs.t5-lc1m4.large"
-      store_gc_type = "c2d-highcpu-4"
+      store_gc_type = "c2d-standard-4"
 
       boot_do_type = "s-2vcpu-4gb"
       boot_ac_type = "ecs.t5-lc1m2.large"
-      boot_gc_type = "c2d-highcpu-2"
+      boot_gc_type = "c2d-standard-2"
 
       db_do_type = "c2-16vcpu-32gb-intel"
       db_ac_type = "ecs.c6.4xlarge"
